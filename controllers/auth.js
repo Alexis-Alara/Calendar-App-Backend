16e17cc1b66040b0a039a44515553483d5a5cc4d
Alexis-Alara <alexis.alaraa@gmail.com>
2023-07-09 18:13:46 -0500

    loginUser validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,34 +22,66 @@ const newUser = async( req, res = response ) => {
 
         // Password encryption
         const salt = bcrypt.genSaltSync();
-        user.password = bcrypt.hashSync( password, salt )
+        user.password = bcrypt.hashSync( password, salt );
 
         await user.save();
+        // JSON Web Token
 
         res.status(201).json({
             ok: true,
             uid: user.id,
             name: user.name
-        })
+        });
 
     } catch (error) {
         res.status(500).json({
             ok: false,
             msg: 'Please talk to an administrator'
-        })   
+        });   
     }
 }
 
-const loginUser = ( req, res = response ) => {
+const loginUser = async( req, res = response ) => {
 
     const { email, password } = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+    try {
+        
+        const user = await User.findOne({ email });
+
+        if( !user ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Invalid user and password'
+            });
+        }
+
+        // Check passwords
+        const validPassword = bcrypt.compareSync( password, user.password );
+
+        if ( !validPassword ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Invalid user and password'
+            });
+        }
+
+        // JSON Web Token
+
+
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        });
+
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Please talk to an administrator'
+        });   
+    }
 }
 
 const renewToken = ( req, res = response ) => {
